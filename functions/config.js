efa53d7703ab477b041d0724ef8b3bd98612a31b
Crean, Patrick <Patrick.Crean@hmhco.com>
2018-05-23 11:20:15 +0100

    remove config.js

--- functions/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  consumer_key: 'UCkb8UpSgYVx2yOrvOZVVFetd',
-  consumer_secret: 'IhTjQr79354idWkG9EYKFDJCModlBavbTWOg2adyEsvBnPmGxB',
-  access_token: '18979957-puhlGlYftylhnx1EBlZxEzgL8KNyGx4ihYROgTWIU',
-  access_token_secret: 'hSKp5qBbXEMWyRFx6IYsg7ddLz1HnnkyJidZH6fkzSd8O',
-  timeout_ms: 60 * 1000
-}
